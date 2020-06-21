26327f783a834e762456b4932f26d1d5a281d2b3
AjayLiu <ajayliu2928@gmail.com>
2020-06-21 00:06:39 -0700

    restyle

--- app.py
+++ app.py
@@ -75,16 +75,19 @@ def upload():
 if __name__ == '__main__':    
     app.run()
 
-# app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-# @app.after_request
-# def add_header(r):
-#     """
-#     Add headers to both force latest IE rendering engine or Chrome Frame,
-#     and also to cache the rendered page for 10 minutes.
-#     """
-#     r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-#     r.headers["Pragma"] = "no-cache"
-#     r.headers["Expires"] = "0"
-#     r.headers['Cache-Control'] = 'public, max-age=0'
-#     return r
+
+# FOR DEVELOPMENT CACHING
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
